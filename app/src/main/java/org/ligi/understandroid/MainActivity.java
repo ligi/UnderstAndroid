e180cb83b06245501c28d18cfe46117bd73711a9
ligi <ligi@ligi.de>
2014-12-25 23:33:02 +0100

    fix a bug thingy

--- app/src/main/java/org/ligi/understandroid/MainActivity.java
+++ app/src/main/java/org/ligi/understandroid/MainActivity.java
@@ -1,12 +1,11 @@
 package org.ligi.understandroid;
 
-import android.graphics.Point;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
-import android.view.Display;
 import android.widget.ImageView;
-
 import com.google.zxing.BarcodeFormat;
+
+import org.ligi.axt.AXT;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
@@ -15,13 +14,14 @@ import butterknife.OnClick;
 
 public class MainActivity extends ActionBarActivity {
 
-
     @InjectView(R.id.barcodeImage)
     ImageView barcodeImage;
 
     @OnClick(R.id.fixABug)
     void fixABug() {
-
+        // this will crash - the log you can access via ADB will tell you what's wrong
+        // the internet can tell you how to fix this ( common ) bug
+        AXT.at(this).startCommonIntent().activityFromClass(FixABugActivity.class);
     }
 
     @Override
@@ -33,7 +33,5 @@ public class MainActivity extends ActionBarActivity {
 
         final int width = getWindowManager().getDefaultDisplay().getWidth();
         barcodeImage.setImageBitmap(BarcodeHelper.generateBarCodeBitmap("https://platform.toto.io/game/31C3#{\"t\":\"Task\",\"k\":\"qOpQc6Osa0sMQeM\",\"a\":\"Solve\",\"s\":\"TsltTX74rPWowQ5\"}", BarcodeFormat.QR_CODE, width));
-
     }
-
 }
